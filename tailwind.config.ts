6b9764409a7e6666a0e2d410f090a918b6230b7a
rohan09-raj <rajrohan1914@gmail.com>
2024-03-12 15:35:07 +0530

    chore: added max-width config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,13 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
