08f75fea6c64fb8f96b374b99d2d9fcca690dab4
rohan09-raj <rajrohan1914@gmail.com>
2024-03-12 19:24:53 +0530

    feat: added features section

---
+++ src/components/Features.tsx
@@ -0,0 +1,30 @@
+import IconCard from "./cards/IconCard";
+
+export default function Features() {
+  const features = [
+    {
+      icon: "https://storage.googleapis.com/getplus.in/images/extraOffer.png",
+      text: "Upto 0.5gm extra instantly",
+    },
+    {
+      icon: "https://storage.googleapis.com/getplus.in/images/redeem.png",
+      text: "Redeem at 300+ partner jewellers",
+    },
+    {
+      icon: "https://storage.googleapis.com/getplus.in/images/12months.png",
+      text: "In 12 months get 1gm extra",
+    },
+    {
+      icon: "https://storage.googleapis.com/getplus.in/images/trusted.png",
+      text: "Trusted by 25,000+ users",
+    },
+  ];
+
+  return (
+    <section className="grid p-12 gap-10 grid-cols-4 lg:grid-cols-2 sm:text-xs md:p-6 sm:gap-2 rounded-t-3xl border-t border-t-[#FFE483] bg-gradient-to-b from-[#FFFAF1]">
+      {features.map((feature, index) => (
+        <IconCard key={index} icon={feature.icon} text={feature.text} />
+      ))}
+    </section>
+  );
+}
---
+++ src/components/cards/IconCard.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+export default function IconCard({
+  icon,
+  text,
+}: {
+  icon: string;
+  text: string;
+}) {
+  return (
+    <div className="flex items-center gap-4 h-28 max-w-xs sm:h-16">
+      <Image src={icon} alt={text} height={48} width={48} />
+      <p className="text-[#000000B3] font-semibold">{text}</p>
+    </div>
+  );
+}
