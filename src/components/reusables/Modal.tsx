7488fa7a69dbe1d7892b67f1319a6c16b8f3029c
rohan09-raj <rajrohan1914@gmail.com>
2024-03-14 15:21:59 +0530

    fix: modal inside click close issue

--- src/components/reusables/Modal.tsx
+++ src/components/reusables/Modal.tsx
@@ -8,12 +8,13 @@ export default function Modal({
   onCloseClick: () => void;
 }) {
   return (
-    <section
-      onClick={onCloseClick}
-      className="w-full h-full fixed top-0 left-0 flex items-center justify-center bg-black bg-opacity-50 z-20"
-    >
-      <section className="flex flex-col p-6 rounded-xl bg-white md:w-full md:h-full md:rounded-none">
-        <button className="flex justify-end" onClick={onCloseClick}>
+    <section className="w-full h-full fixed top-0 left-0 flex items-center justify-center z-20">
+      <div
+        onClick={onCloseClick}
+        className="w-full h-full fixed top-0 left-0 bg-black bg-opacity-50"
+      ></div>
+      <section className="w-1/2 h-1/2 flex flex-col p-6 rounded-xl bg-white md:w-full md:h-full md:rounded-none z-10">
+        <button className="flex justify-end " onClick={onCloseClick}>
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
