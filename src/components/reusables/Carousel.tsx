6cf1ca74261cc8d469f8ce89376653df8331b24f
rohan09-raj <rajrohan1914@gmail.com>
2024-03-14 23:44:32 +0530

    feat: added custom carousel arrows

--- src/components/reusables/Carousel.tsx
+++ src/components/reusables/Carousel.tsx
@@ -1,10 +1,10 @@
 "use client";
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic";
 import "react-multi-carousel/lib/styles.css";
- 
+
 const Carousel = dynamic(() => import("react-multi-carousel"), {
   loading: () => <p>Loading...</p>,
-})
+});
 
 export default function CustomCarousel({
   children,
@@ -32,6 +32,8 @@ export default function CustomCarousel({
 
   return (
     <Carousel
+      customLeftArrow={<CustomLeftArrow />}
+      customRightArrow={<CustomRightArrow />}
       swipeable={true}
       draggable={true}
       showDots={false}
@@ -44,3 +46,45 @@ export default function CustomCarousel({
     </Carousel>
   );
 }
+
+const CustomLeftArrow = ({ onClick }: { onClick?: () => void }) => {
+  return (
+    <button className="absolute -left-3 z-10 cursor-pointer" onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-10 h-10"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M15.75 19.5 8.25 12l7.5-7.5"
+        />
+      </svg>
+    </button>
+  );
+};
+
+const CustomRightArrow = ({ onClick }: { onClick?: () => void }) => {
+  return (
+    <button className="absolute -right-3 z-10 cursor-pointer" onClick={onClick}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-10 h-10"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m8.25 4.5 7.5 7.5-7.5 7.5"
+        />
+      </svg>
+    </button>
+  );
+};
